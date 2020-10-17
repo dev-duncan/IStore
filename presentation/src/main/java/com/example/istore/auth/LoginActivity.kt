7cc5ea268f60a7e27d7eb3eded300600bef7163c
duncanbabakedarius <duncangkariuki@gmail.com>
2020-10-17 05:10:47 +0300

    login added

--- presentation/src/main/java/com/example/istore/auth/LoginActivity.kt
+++ presentation/src/main/java/com/example/istore/auth/LoginActivity.kt
@@ -1,7 +1,13 @@
 package com.example.istore.auth
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.istore.R
 
 class LoginActivity : AppCompatActivity() {
+    override fun onCreate( savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
 
 }
